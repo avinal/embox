96a261321e269cedd9ed0288330e39e826ad84c5
Anton Bondarev <anton.bondarev2310@gmail.com>
2020-12-07 16:38:55 +0300

    drivers: Add stm32h7 support to stm32usart driver

---
+++ src/drivers/serial/stm32_usart/stm32_callbacks.c
@@ -0,0 +1,24 @@
+/**
+ * @file
+ *
+ * @date Dec 7, 2020
+ * @author Anton Bondarev
+ */
+
+#include <drivers/serial/stm_usart.h>
+
+void HAL_UART_ErrorCallback(UART_HandleTypeDef *UartHandle) {
+
+}
+
+void HAL_UARTEx_RxFifoFullCallback(UART_HandleTypeDef *UartHandle) {
+
+}
+
+void HAL_UARTEx_TxFifoEmptyCallback(UART_HandleTypeDef *UartHandle) {
+
+}
+
+void HAL_UARTEx_WakeupCallback(UART_HandleTypeDef *UartHandle) {
+
+}
